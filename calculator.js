316559157cf85ff0cbcdfe18d22fbedf0cf808cc
Muhammad Hasan <hasan@sirus.dev>
2019-10-09 13:23:22 +0700

    tambahin UI & kontroler kalkulator

--- calculator.js
+++ calculator.js
@@ -1,36 +1,54 @@
-// document.addEventListener("mousemove", (moveEvent) => {
-//   console.log(moveEvent.clientX, moveEvent.clientY);
-// });
+const { fromEvent, merge } = rxjs;
+const { map, takeUntil, tap } = rxjs.operators;
 
-/*
-1. kotaknya di klik
-2. mousenya di geser
-3. tombolnya di lepas dari kotak
-*/
+const numbers = document.querySelectorAll(".angka>button");
 
+const operands = document.querySelectorAll(".operand>button");
 
-const box = document.getElementById("box");
+const plus = operands[0];
 
-// kotaknya di klik
-box.addEventListener('mousedown', (start) => {
-  // geser kotak dari event mousemove
-  const onMouseMove = (move) => {
-    // ngilangin highlight di text
-    move.preventDefault();
-  
-    const positionTop = move.clientY - start.offsetY;
-    const positionLeft = move.clientX - start.offsetX;
-    box.style.top = positionTop + "px";
-    box.style.left = positionLeft + "px";
-  };
+const zero$ = fromEvent(numbers[0], "click").pipe(map(() => 0));
+const one$ = fromEvent(numbers[1], "click").pipe(map(() => 1));
+const two$ = fromEvent(numbers[2], "click").pipe(map(() => 2));
+const three$ = fromEvent(numbers[3], "click").pipe(map(() => 3));
+const four$ = fromEvent(numbers[4], "click").pipe(map(() => 4));
+const five$ = fromEvent(numbers[5], "click").pipe(map(() => 5));
+const six$ = fromEvent(numbers[6], "click").pipe(map(() => 6));
+const seven$ = fromEvent(numbers[7], "click").pipe(map(() => 7));
+const eight$ = fromEvent(numbers[8], "click").pipe(map(() => 8));
+const nine$ = fromEvent(numbers[9], "click").pipe(map(() => 9));
 
-  // pas digerakin, boxnya ikut geser
-  document.addEventListener('mousemove', onMouseMove);
+const plus$ = fromEvent(plus, "click").pipe(map(() => "plus"));
 
-  // pas tombolnya di angkat
-  box.addEventListener('mouseup', () => {
-    // hapus listener mousemove
-    document.removeEventListener('mousemove', onMouseMove);
-  });
-});
+const numbers$ = merge(
+  zero$,
+  one$,
+  two$,
+  three$,
+  four$,
+  six$,
+  seven$,
+  eight$,
+  nine$
+);
 
+const operators$ = merge(plus$);
+
+let number1 = '';
+let number2 = '';
+let operand = null;
+
+const calculator$ = merge(
+  numbers$,
+  operators$
+).pipe(
+  tap((number) => {
+    number1 = number1 + number
+  }),
+  takeUntil(operators$),
+  map((operator) => {
+    return operator;
+  })
+)
+
+calculator$.subscribe(calc => console.log(calc));
