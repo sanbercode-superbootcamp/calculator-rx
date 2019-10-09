b75bdb9ec884d066b780e1c9bdc07786459a3ca1
Muhammad Hasan <hasan@sirus.dev>
2019-10-09 10:19:09 +0700

    buat fungsi eser2 kotak pake event driven programming

--- calculator.js
+++ calculator.js
@@ -1,5 +1,36 @@
-document.addEventListener("mousemove", (moveEvent) => {
-  console.log(moveEvent.clientX, moveEvent.clientY);
-});
+// document.addEventListener("mousemove", (moveEvent) => {
+//   console.log(moveEvent.clientX, moveEvent.clientY);
+// });
 
+/*
+1. kotaknya di klik
+2. mousenya di geser
+3. tombolnya di lepas dari kotak
+*/
+
+
+const box = document.getElementById("box");
+
+// kotaknya di klik
+box.addEventListener('mousedown', (start) => {
+  // geser kotak dari event mousemove
+  const onMouseMove = (move) => {
+    // ngilangin highlight di text
+    move.preventDefault();
+  
+    const positionTop = move.clientY - start.offsetY;
+    const positionLeft = move.clientX - start.offsetX;
+    box.style.top = positionTop + "px";
+    box.style.left = positionLeft + "px";
+  };
+
+  // pas digerakin, boxnya ikut geser
+  document.addEventListener('mousemove', onMouseMove);
+
+  // pas tombolnya di angkat
+  box.addEventListener('mouseup', () => {
+    // hapus listener mousemove
+    document.removeEventListener('mousemove', onMouseMove);
+  });
+});
 
