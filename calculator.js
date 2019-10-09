33a25bedfc871fe1f8970250ee6d38a60fcd3596
Rismawati YF <rismafauziah130399@gmail.com>
2019-10-09 10:19:25 +0700

    geser-geser kotak pake event driver

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
-document.addEventListener("mousemove", (moveEvent) => {
-    console.log(moveEvent.clientX, moveEvent.clientY)
+// document.addEventListener("mousemove", (moveEvent) => {
+//     console.log(moveEvent.clientX, moveEvent.clientY)
+// })
+
+const box = document.getElementById("box")
+
+box.addEventListener('mousedown', (start) => {
+    const onMouseMove = (move) => {
+        move.preventDefault()
+        const positionTop = move.clientY - start.offsetY
+        const positionLeft = move.clientX - start.offsetX
+        box.style.top = positionTop + "px"
+        box.style.left = positionLeft + "px"
+    }
+
+    document.addEventListener('mousemove', onMouseMove)
+    box.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove)
+    })
 })
