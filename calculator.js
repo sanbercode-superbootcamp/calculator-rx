59addfe95c8a31271a9d0cac2fc2680284381936
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-09 10:12:16 +0700

    add box and listener when click

--- calculator.js
+++ calculator.js
@@ -2,3 +2,19 @@ document.addEventListener('mousemove', (moveEvent) => {
     console.log(moveEvent.clientX, moveEvent.clientY);
 });
 
+/*
+1. kotaknya di klik
+2. mousenya di geser
+3. tombolnya di lepas dari kotak
+*/
+
+const box = document.getElementById("box");
+
+box.addEventListener('mousedown', (start) => {
+    document.addEventListener('mousemove', (move) => {
+        const positionTop = move.clientY - start.offsetY;
+        const positionLeft = move.clientX - start.offsetX;
+        box.style.top = positionTop + "px";
+        box.style.left = positionLeft + "px";
+    });
+});
